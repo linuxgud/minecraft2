552fb85f316992c081f251c23bffb0b89d3258ae
linux <linux@LAPTOP-M2S0RJTS>
2018-09-15 19:18:47 +0200

    z

--- minecraft/manifests/install.pp
+++ minecraft/manifests/install.pp
@@ -1,5 +1,5 @@
 class minecraft::install {
-  package { 'java-1.8.0-openjdk-devel': ensure => "1.8.0.161-0.b14.el7_4", }
+  package { 'java-1.8.0-openjdk-devel': ensure => "1.8.0.181-3.b13.el7_5", }
 
   package { ['screen', 'js', 'wget']: }
 
